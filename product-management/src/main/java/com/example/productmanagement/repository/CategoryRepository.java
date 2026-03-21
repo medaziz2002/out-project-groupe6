c8c75e0cdb4dde454eed5b83122ebde7c771ddf7
medaziz2002 <azizhassine673@gmail.com>
2026-03-21 22:24:37 +0100

    chore: remove backend from frontend branch

--- product-management/src/main/java/com/example/productmanagement/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.productmanagement.repository;
-
-import com.example.productmanagement.entity.Category;
-import com.example.productmanagement.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- product-management/src/main/java/com/example/productmanagement/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.productmanagement.repository;
-
-import com.example.productmanagement.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
