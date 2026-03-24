4702ec1a0b8272792e42389de56c6431eb04cf4e
safae-bellafkih <safae.bellafkih.2005@gmail.com>
2026-03-24 18:02:53 +0100

    Update routes: add update-product route

---
+++ front-end/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { ProductsComponent } from './produits/produits.component';
+import { AddProductComponent } from './add-produit/add-produit.component';
+import { SearchByCategoryComponent } from './recherche-par-categorie/recherche-par-categorie.component';
+import { SearchByNameComponent } from './recherche-par-nom/recherche-par-nom.component';
+import { ListCategoriesComponent } from './liste-categories/liste-categories.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: 'search-by-category', component: SearchByCategoryComponent },
+  { path: 'search-by-name', component: SearchByNameComponent },
+  { path: 'categories', component: ListCategoriesComponent },
+  { path: 'update-product/:id', component: UpdateProductComponent},
+];
